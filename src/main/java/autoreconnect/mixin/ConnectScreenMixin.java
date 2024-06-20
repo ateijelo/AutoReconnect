79b0d28127c9f228a628fbe95c16fc56d281455f
NotRyken <notryken@gmail.com>
2024-06-20 10:25:48 +0800

    Update to mc1.20.6

--- src/main/java/autoreconnect/mixin/ConnectScreenMixin.java
+++ src/main/java/autoreconnect/mixin/ConnectScreenMixin.java
@@ -4,6 +4,7 @@ import autoreconnect.AutoReconnect;
 import autoreconnect.reconnect.MultiplayerReconnectStrategy;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
+import net.minecraft.client.network.CookieStorage;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,9 +14,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ConnectScreen.class)
 public class ConnectScreenMixin {
-    @Inject(at = @At("HEAD"), method = "connect(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;)V")
-    private void connect(MinecraftClient client, ServerAddress address, ServerInfo serverInfo, CallbackInfo info) {
+    @Inject(at = @At("HEAD"), method = "connect(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;Lnet/minecraft/client/network/CookieStorage;)V")
+    private void connect(MinecraftClient client, ServerAddress address, ServerInfo serverInfo, CookieStorage cookieStorage, CallbackInfo ci) {
         if (serverInfo == null) return;
-        AutoReconnect.getInstance().setReconnectHandler(new MultiplayerReconnectStrategy(serverInfo));
+        AutoReconnect.getInstance().setReconnectHandler(new MultiplayerReconnectStrategy(serverInfo, cookieStorage));
     }
 }
--- src/main/java/autoreconnect/reconnect/MultiplayerReconnectStrategy.java
+++ src/main/java/autoreconnect/reconnect/MultiplayerReconnectStrategy.java
@@ -4,14 +4,17 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.network.CookieStorage;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
 
 public class MultiplayerReconnectStrategy extends ReconnectStrategy {
     private final ServerInfo serverInfo;
+    private final CookieStorage cookieStorage;
 
-    public MultiplayerReconnectStrategy(ServerInfo serverInfo) {
+    public MultiplayerReconnectStrategy(ServerInfo serverInfo, CookieStorage cookieStorage) {
         this.serverInfo = serverInfo;
+        this.cookieStorage = cookieStorage;
     }
 
     @Override
@@ -25,10 +28,11 @@ public class MultiplayerReconnectStrategy extends ReconnectStrategy {
     @Override
     public void reconnect() {
         ConnectScreen.connect(
-            new MultiplayerScreen(new TitleScreen()),
-            MinecraftClient.getInstance(),
-            ServerAddress.parse(serverInfo.address),
-            serverInfo,
-            false);
+                new MultiplayerScreen(new TitleScreen()),
+                MinecraftClient.getInstance(),
+                ServerAddress.parse(serverInfo.address),
+                serverInfo,
+                false,
+                cookieStorage);
     }
 }
